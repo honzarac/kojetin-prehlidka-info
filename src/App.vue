43291be9627acd6e4648abc8701f46ad9eab995d
Honza Raclavský <janraclav@gmail.com>
2022-03-08 22:58:37 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <layout>
+    <MainSlider/>
+  </layout>
+</template>
+
+<script>
+import MainSlider from './components/MainSlider.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MainSlider
+  }
+}
+</script>
+
+<style>
+body, html {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ src/components/MainSlider.vue
@@ -0,0 +1,41 @@
+<template>
+  <Splide :options="{ rewind: true }">
+    <SplideSlide>
+      <img src="../assets/shows/sheldon/245824521_5085846968098340_2359551967150374682_n.jpg" alt="Sample 1">
+    </SplideSlide>
+    <SplideSlide>
+      <img src="../assets/shows/sheldon/245824521_5085846968098340_2359551967150374682_n.jpg" alt="Sample 2">
+    </SplideSlide>
+  </Splide>
+</template>
+
+<script>
+import {Splide, SplideSlide} from '@splidejs/vue-splide'
+import '@splidejs/splide/dist/css/splide.min.css'
+
+export default {
+  name: 'MainSlider',
+  components: {
+    Splide,
+    SplideSlide
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
